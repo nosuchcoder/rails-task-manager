7b75802326d7c7eeef9b3550758513181ca29eb8
Kwame Dalrymple <neokwame@tutanota.com>
2020-01-18 16:38:59 +0000

    Functionality working / re-fractor needed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to tasks_path(@task)
   end
 
   def destroy
